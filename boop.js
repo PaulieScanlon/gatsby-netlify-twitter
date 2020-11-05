4148d4fe0c3848e3f4c0d78b284881a034dd4b4b
pauliescanlon <pauliescanlon@gmail.com>
2020-11-05 22:35:48 +0000

    add Twitter v2

--- boop.js
+++ boop.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const Twitter = require('twitter-v2')
 
 const client = new Twitter({
@@ -7,7 +8,7 @@ const client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 })
 
-exports.handler = async (event, context, callback) => {
+module.exports = async () => {
   const { data, errors } = await client.get('tweets/search/recent', {
     query: 'PaulieScanlon',
     max_results: '100',
@@ -16,12 +17,8 @@ exports.handler = async (event, context, callback) => {
         'author_id,context_annotations,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang,possibly_sensitive,public_metrics,referenced_tweets,source,text,withheld',
     },
   })
-
-  callback(null, {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-    statusCode: 200,
-    body: data,
-  })
+  console.log('errors:', errors)
+  console.log('data:', data)
 }
+
+require('make-runnable')
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,28 @@
-import React, { FunctionComponent, useEffect } from 'react'
+import React, { FunctionComponent, useEffect, useState } from 'react'
 
 const IndexPage: FunctionComponent = () => {
+  const [data, setData] = useState('')
+
   useEffect(() => {
     fetch('https://brandcamp.netlify.app/.netlify/functions/twitter')
       .then((response) => response.text())
       .then((data) => {
         console.log('data:', data)
+        setData(data)
       })
       .catch((error) => {
         console.log('error: ', error)
       })
   }, [])
 
-  return <main>Index</main>
+  return (
+    <main>
+      <h1>Index</h1>
+      <pre>
+        <code>{JSON.stringify(data, null, 2)}</code>
+      </pre>
+    </main>
+  )
 }
 
 export default IndexPage
